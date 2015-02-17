944672447052006283a281787a3fcf3d952b4494
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2015-02-17 07:03:49 +0000

    'Completed tasks 1 through 5.'

--- task_02.py
+++ task_02.py
@@ -4,3 +4,13 @@
 
 
 import inquisition
+
+LEN_SPAN = len('Spanish')
+
+INDEX_SPAN = inquisition.SPANISH.index('Spanish')
+
+SLICE_LT = inquisition.SPANISH[0:INDEX_SPAN]
+
+SLICE_RT = inquisition.SPANISH[INDEX_SPAN + LEN_SPAN:]
+
+FLEMISH = SLICE_LT + 'Flemish' + SLICE_RT
